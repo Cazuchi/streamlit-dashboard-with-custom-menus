3977865c0d98383eb415f927520f8ac062b7df02
Mike <mikeehansen@gmail.com>
2026-09-22 15:40:59 +0200

    Formatting + added storby functionality

--- steps/initial_menu.py
+++ steps/initial_menu.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
 def render_page():
-    st.write("### Choose Filter Mode")
+    st.write("### Vælg data:")
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        if st.button("Filter by Municipality", use_container_width=True):
+        if st.button("Overnatninger filtreret på kommune", use_container_width=True):
             st.session_state.filter_type = "municipality"
             st.session_state.step = 2
             st.rerun()
             
     with col2:
-        if st.button("Filter by Destination", use_container_width=True):
+        if st.button("Overnatninger filtreret på destination", use_container_width=True):
             st.session_state.filter_type = "destination"
             st.session_state.step = 2
             st.rerun()
 
     with col3:
-        if st.button("Filter by Region", use_container_width=True):
+        if st.button("Overnatninger filtreret på region", use_container_width=True):
             st.session_state.filter_type = "region"
             st.session_state.step = 2
             st.rerun()
@@ -33,7 +33,7 @@ def render_page():
     }
 
     with col1:
-        if st.button("Filter by storby", use_container_width=True):
+        if st.button("Storbyovernatninger", use_container_width=True):
             st.session_state.filter_type = "storby"
             st.session_state.selected_diskretioneret_option = "excl"
             active_dict = STORBY_OPTIONS
